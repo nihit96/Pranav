1f4a50f9c6ea3fd7aa7e5306e6d0f3528b1253b9
Nihit Jain <nihit.jain@accoliteindia.com>
2020-01-10 17:01:24 +0530

    sum logic

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,11 @@
-Class Calculator {}
+import java.util.Scanner; 
+public class Calculator{
+ public static void main(String[] args) {
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		int sum = a+b;
+		System.Out.Println("Sum is :-" , sum);
+	}
+}
