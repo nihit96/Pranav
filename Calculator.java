96fc63c60838fd17776873bd4dacd9767e5d40d7
Yogesh Bhakhar <Yogesh.bhakhar@accoliteindia.com>
2020-01-10 17:04:40 +0530

    Addition and Subtraction Function

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,12 @@
-Class Calculator {}
+import java.util.Scanner;
+Class Calculator {
+
+ static void Addition( int firstNumber, int secondNumber ) {
+        System.out.println("Addition = " + (firstNumber + secondNumber));
+    }
+
+
+ static void Substratcion( int firstNumber, int secondNumber ) {
+        System.out.println("Substratcion = " + (firstNumber - secondNumber));
+    }
+}
