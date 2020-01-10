0b6f69d7eb179fe57d25656fadf7e7ade9312819
Kanishk agarwal <kanishk.agarwal@accoliteindia.com>
2020-01-10 17:03:23 +0530

    Kanishk edit

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,14 @@
-Class Calculator {}
+import java.lang.*;
+import java.util*;
+class Calculator {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println(mod(a,b));
+	}
+	double mod(int a,int b){
+		return a%b;
+	}
+
+}
